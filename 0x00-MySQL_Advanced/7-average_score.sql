bfbdd2b55896f2f0677392bcd1437d0e1ca95be5
emmanueldavids <emmanueldavids417@gmail.com>
2023-08-14 11:48:34 +0000

    SQL script that creates a stored procedure ComputeAverageScoreForUser that computes and store the average

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -3,25 +3,21 @@
 
 DELIMITER //
 
-CREATE PROCEDURE ComputeAverageScoreForUser(
-    IN user_id INT
-)
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
     DECLARE total_score FLOAT;
-    DECLARE num_corrections INT;
-    
-    -- Compute total score and number of corrections for the user
-    SELECT SUM(score), COUNT(*) INTO total_score, num_corrections
+    DECLARE total_projects INT;
+
+    SELECT SUM(score), COUNT(*) INTO total_score, total_projects
     FROM corrections
     WHERE user_id = user_id;
-    
-    -- Update average score for the user
-    UPDATE users
-    SET average_score = IF(num_corrections > 0, total_score / num_corrections, 0)
-    WHERE id = user_id;
-    
+
+    IF total_projects > 0 THEN
+        UPDATE users
+        SET average_score = total_score / total_projects
+        WHERE id = user_id;
+    END IF;
 END;
 //
 
 DELIMITER ;
-
