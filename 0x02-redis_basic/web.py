9ab23f827d0b2f64027dba1f39a344d713c5a331
emmanueldavids <emmanueldavids417@gmail.com>
2023-11-01 19:08:27 +0000

    added files

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,36 +1,38 @@
-#!/usr/bin/env python3
 import requests
-import time
-from functools import wraps
-from typing import Dict
+from cachetools import TTLCache
 
-cache: Dict[str, str] = {}
+# Create a cache with an expiration time of 10 seconds
+cache = TTLCache(maxsize=100, ttl=10)
 
 
 def get_page(url: str) -> str:
+    # Check if the URL is already cached
     if url in cache:
-        print(f"Retrieving from cache: {url}")
+        # Return the cached content
         return cache[url]
-    else:
-        print(f"Retrieving from web: {url}")
-        response = requests.get(url)
-        result = response.text
-        cache[url] = result
-        return result
+
+    # If not cached, make a request to the URL
+    response = requests.get(url)
+    if response.status_code == 200:
+        content = response.text
+
+        # Store the content in the cache with the URL as the key
+        cache[url] = content
+
+        # Increment the count for this URL
+        count_key = f"count:{url}"
+        count = cache.get(count_key, 0)
+        cache[count_key] = count + 1
+
+        return content
+
+    return f"Failed to fetch URL: {url}"
 
 
-def cache_with_expiration(expiration: int):
-    def decorator(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            url = args[0]
-            key = f"count:{url}"
-            if key in cache:
-                count, timestamp = cache[key]
-                if time.time() - timestamp > expiration:
-                    result = func(*args, **kwargs)
-                    cache[key] = (count+1, time.time())
-                    return result
-                else:
-                    cache[key] = (count+1, timestamp)
-                    return
+if __name__ == "__main__":
+    url = "http://slowwly.robertomurray.co.uk/delay/5000/url/http://www.example.com"
+
+    for _ in range(5):
+        print(get_page(url))
+
+    print(f"Access count for {url}: {cache.get(f'count:{url}', 0)}")
